85516f50e25f83fd94d6ed28b329dbdad36f971a
J <romanolsen@hotmail.com>
2024-09-14 20:31:50 +0200

    updated sorcerer for metric

--- src/utils.py
+++ src/utils.py
@@ -77,3 +77,39 @@ def generate_hash_id(
     # Generate a unique hash ID
     hash_id = hashlib.md5(hash_input.encode()).hexdigest()
     return hash_id
+
+def compute_residuals(model_forecasts, test_data, min_forecast_horizon):
+    """
+    Compute the forecast error metrics for multiple models.
+
+    Parameters:
+    - model_forecasts: List of forecasted values from different models. Each element is a list of forecasts for each column.
+    - test_data: DataFrame containing the actual values for the forecast period.
+    - min_forecast_horizon: The minimum forecast horizon to consider for each model.
+
+    Returns:
+    - List of DataFrames, each containing the forecast errors for a model.
+    """
+    metrics_list = []
+
+    # Iterate over each model's forecasts
+    for forecasts in model_forecasts:
+        # Initialize an empty DataFrame to store forecast errors for the current model
+        errors_df = pd.DataFrame(columns=test_data.columns)
+        
+        # Calculate forecast errors for each column
+        for i, column in enumerate(test_data.columns):
+            # Get the forecast horizon for the current column
+            forecast_horizon = len(forecasts[i].values)
+            
+            # Calculate forecast errors: actual values minus forecasted values
+            errors_df[column] = test_data[column].values[-forecast_horizon:] - forecasts[i].values
+        
+        # Keep only the first `min_forecast_horizon` rows and reset index
+        truncated_errors_df = errors_df.iloc[:min_forecast_horizon].reset_index(drop=True)
+        
+        # Append the DataFrame of errors for the current model to the list
+        metrics_list.append(truncated_errors_df)
+    stacked = pd.concat(metrics_list, axis=0)
+    
+    return stacked
